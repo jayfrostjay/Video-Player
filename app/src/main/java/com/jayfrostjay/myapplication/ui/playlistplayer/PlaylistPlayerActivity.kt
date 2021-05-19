253a2c18195541cdfc8faf6702448f99196095fa
Jayson Garcia <jayfrostgarcia@gmail.com>
2021-05-19 15:02:55 +0800

    Setup for Playlist Player UI

--- app/src/main/java/com/jayfrostjay/myapplication/ui/playlistplayer/PlaylistPlayerActivity.kt
+++ app/src/main/java/com/jayfrostjay/myapplication/ui/playlistplayer/PlaylistPlayerActivity.kt
@@ -1,26 +1,41 @@
 package com.jayfrostjay.myapplication.ui.playlistplayer
 
+import android.R
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.jayfrostjay.myapplication.databinding.PlaylistListingBinding
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.jayfrostjay.myapplication.databinding.ActivityPlayerBinding
+
 
 class PlaylistPlayerActivity: AppCompatActivity() {
 
-    private lateinit var binding: PlaylistListingBinding
+    private lateinit var binding: ActivityPlayerBinding
+    private var url: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        binding = PlaylistListingBinding.inflate(layoutInflater)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        url = intent.getStringExtra(KEY_VID_URL) ?: ""
+        Log.d("TESTING", url.toString())
 
         initView()
     }
 
     private fun initView(){
 
+        val player = SimpleExoPlayer.Builder(this).build()
+        binding.player.player = player
+
+        val mediaItem: MediaItem = MediaItem.fromUri(url)
+        player.setMediaItem(mediaItem)
+
+        player.prepare()
     }
 
     companion object {
