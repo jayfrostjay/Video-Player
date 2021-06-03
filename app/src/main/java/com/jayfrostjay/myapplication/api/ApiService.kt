1e2784f5f3dc9cc4967fbbd3644499e6bc2949f5
Jayson Garcia <jayfrostgarcia@gmail.com>
2021-06-03 14:27:11 +0800

    API Logging Interceptor

--- app/src/main/java/com/jayfrostjay/myapplication/api/ApiService.kt
+++ app/src/main/java/com/jayfrostjay/myapplication/api/ApiService.kt
@@ -2,6 +2,7 @@ package com.jayfrostjay.myapplication.api
 
 import com.jayfrostjay.myapplication.data.Playlist
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,11 +17,16 @@ class ApiService {
             val builder = OkHttpClient.Builder().also {
                 it.addInterceptor { chain ->
                     val requestBuilder = chain.request().newBuilder()
-                        .header("AUTHORIZATION", "")
-
+                        .header("Authorization", "TESTING")
                     chain.proceed(requestBuilder.build())
                 }
             }
+
+            val interceptor = HttpLoggingInterceptor().also {
+                it.level = HttpLoggingInterceptor.Level.HEADERS
+                it.level = HttpLoggingInterceptor.Level.BODY
+            }
+            builder.addInterceptor(interceptor)
 
             return builder.build()
         }
@@ -28,6 +34,7 @@ class ApiService {
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://quipper.github.io/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
         .build()
 
     fun <T> createService(service: Class<T>): T {
